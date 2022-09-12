6044703fc645622e7c9da2157f99aa9657585e6d
l3002 <111552820+l3002@users.noreply.github.com>
2022-09-12 14:58:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.io.*;
 public class Main{
-  public static void main(String[] args){
+  public static void main(String[] args) throws Exception {
     System.out.println("This is a Simple Java- Program");
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter your Name:");
+    String name = br.readLine();
+    System.out.println("Enter the City:");
+    String city = br.readLine();
+    System.out.println("Enter your Mobile Number:");
+    long mnumber= Long.parseLong(br.readLine());
+    User u = new User(name,city,mnumber);
+    u.toString();
   }
 }
